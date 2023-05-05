ceec6c98b8868104ee48e29ae236f611eadfbad7
Avery Yang <averyyang2674@gmail.com>
2023-05-05 14:24:58 -0500

    Revert "set units to imperial"
    This reverts commit 784b881decb508b293b4b5bcf394932786aa2482.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
-    const [query, setQuery] = useState({ q: 'minneapolis' });
-    const [units, setUnits] = useState({ units: 'imperial' });
-    const [weather, setWeather] = useState(null);
+	const [query, setQuery] = useState({ q: 'minneapolis' });
+	const [units, setUnits] = useState({ units: 'imperial' });
+	const [weather, setWeather] = useState(null);
 
 	//see what's inside weather => coming from our api
 	console.log(weather);
@@ -24,20 +24,20 @@ export default function App() {
 			const message = query.q ? query.q : 'current location.';
 			toast.info(`Fetching weather for ${message}`);
 
-            //getWeather
-            await getFormattedWeatherData({ ...query, units: 'imperial' }).then(
-                (data) => {
-                    //toastify SUCCESS
-                    toast.success(
-                        `Successfully fetched weather for ${data.name}, ${data.country}`
-                    );
-                    setWeather(data);
-                }
-            );
-        };
-        fetchWeather();
-        //every time query or units change, fetch new data
-    }, [query, units]);
+			//getWeather
+			await getFormattedWeatherData({ ...query, units }).then(
+				(data) => {
+					//toastify SUCCESS
+					toast.success(
+						`Successfully fetched weather for ${data.name}, ${data.country}`
+					);
+					setWeather(data);
+				}
+			);
+		};
+		fetchWeather();
+		//every time query or units change, fetch new data
+	}, [query, units]);
 
 	//conditional background based on weather temperature
 	const formatBackground = () => {
@@ -50,6 +50,16 @@ export default function App() {
 		//else weather is warm...
 		return 'from-yellow-700 to to-orange-700';
 	};
+    //conditional background based on weather temperature
+    const formatBackground = () => {
+        //standard background
+        if (!weather) return 'from-cyan-500 to to-blue-900';
+        const threshold = units === 'metric' ? 20 : 60;
+        //if weather is cold...
+        if (weather.temp <= threshold) return 'from-cyan-500 to to-blue-900';
+        //else weather is warm...
+        return 'from-yellow-700 to to-orange-700';
+    };
 
 	return (
 		<div
