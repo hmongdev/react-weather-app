a71905826f496b88b4228708b40a7cd05d4bcd9f
Avery Yang <averyyang2674@gmail.com>
2023-05-05 14:09:53 -0500

    fix api call

--- src/App.js
+++ src/App.js
@@ -3,72 +3,85 @@ import Inputs from './components/Inputs';
 import CityButtons from './components/CityButtons';
 import TimeLocation from './components/TimeLocation';
 import TempDetails from './components/TempDetails';
-import Forecast from './components/Forecast';
+// import Forecast from './components/Forecast';
 import getFormattedWeatherData from './services/weatherService';
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
-    const [query, setQuery] = useState({ q: 'minneapolis' });
-    const [units, setUnits] = useState('imperial');
-    const [weather, setWeather] = useState(null);
+	const [query, setQuery] = useState({ q: 'minneapolis' });
+	const [units, setUnits] = useState('imperial');
+	const [weather, setWeather] = useState(null);
 
-    //see what's inside weather => coming from our api
-    console.log(weather);
+	//see what's inside weather => coming from our api
+	console.log(weather);
 
-    useEffect(() => {
-        //on load, fetch weather data
-        const fetchWeather = async () => {
-            //toastify FETCH
-            const message = query.q ? query.q : 'current location.';
-            toast.info(`Fetching weather for ${message}`);
+	useEffect(() => {
+		//on load, fetch weather data
+		const fetchWeather = async () => {
+			//toastify FETCH
+			const message = query.q ? query.q : 'current location.';
+			toast.info(`Fetching weather for ${message}`);
 
-            console.log(units);
+			console.log(units);
 
-            //getWeather
-            await getFormattedWeatherData({ ...query, units }).then((data) => {
-                //toastify SUCCESS
-                toast.success(
-                    `Successfully fetched weather for ${data.name}, ${data.country}`
-                );
-                setWeather(data);
-            });
-        };
-        fetchWeather();
-        //every time query or units change, fetch new data
-    }, [query, units]);
+			//getWeather
+			await getFormattedWeatherData({ ...query, units }).then(
+				(data) => {
+					//toastify SUCCESS
+					toast.success(
+						`Successfully fetched weather for ${data.name}, ${data.country}`
+					);
+					setWeather(data);
+				}
+			);
+		};
+		fetchWeather();
+		//every time query or units change, fetch new data
+	}, [query, units]);
 
-    //conditional background based on weather temperature
-    const formatBackground = () => {
-        //standard background
-        if (!weather) return 'from-cyan-500 to to-blue-900';
-        const threshold = units === 'imperial' ? 20 : 60;
-        //if weather is cold...
-        if (weather.temp <= threshold) return 'from-cyan-500 to to-blue-900';
-        //else weather is warm...
-        return 'from-yellow-700 to to-orange-700';
-    };
+	//conditional background based on weather temperature
+	const formatBackground = () => {
+		//standard background
+		if (!weather) return 'from-cyan-500 to to-blue-900';
+		const threshold = units === 'imperial' ? 20 : 60;
+		//if weather is cold...
+		if (weather.temp <= threshold)
+			return 'from-cyan-500 to to-blue-900';
+		//else weather is warm...
+		return 'from-yellow-700 to to-orange-700';
+	};
 
-    return (
-        <div
-            className={`mx-auto py-10 px-10 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
-        >
-            <CityButtons setQuery={setQuery} />
-            <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
-            {weather && (
-                <>
-                    <TimeLocation weather={weather} />
-                    <TempDetails weather={weather} />
-                    <Forecast title="hourly forecast" items={weather.hourly} />
-                    <Forecast title="daily forecast" items={weather.daily} />
-                </>
-            )}
-            <ToastContainer
-                autoClose={5000}
-                theme="colored"
-                newestOnTop={true}
-            />
-        </div>
-    );
+	return (
+		<div
+			className={`h-screen mx-auto py-10 px-10 bg-gradient-to-br shadow-xl shadow-gray-400 ${formatBackground()}`}
+		>
+			<CityButtons setQuery={setQuery} />
+			<Inputs
+				setQuery={setQuery}
+				units={units}
+				setUnits={setUnits}
+			/>
+			{weather && (
+				<>
+					<TimeLocation weather={weather} />
+					<TempDetails weather={weather} />
+					{/* <Forecast
+						title="hourly forecast"
+						items={weather.hourly}
+					/> */}
+					{/* <Forecast
+						title="daily forecast"
+						items={weather.daily}
+					/> */}
+				</>
+			)}
+			<ToastContainer
+				autoClose={5000}
+				theme="colored"
+				newestOnTop={true}
+			/>
+		</div>
+	);
 }
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,99 +1,82 @@
 import { DateTime } from 'luxon';
 
-const API_KEY = 'f14168772b3153560591c8afcc9481e3';
-const BASE_URL = 'https://pro.openweathermap.org/data/2.5';
+// https://api.openweathermap.org/data/2.5/weather?lat=44.98&lon=-93.263&&appid=b601b84d0e7ce5864247288c5731f8ae&units=imperial
 
-// https://pro.openweathermap.org/data/2.5/onecall?lat=44.98&lon=-93.2638&exclude=current,minutely,hourly,alerts&appid=f14168772b3153560591c8afcc9481e3&units=imperial
+const API_KEY = 'b601b84d0e7ce5864247288c5731f8ae';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
-const getWeatherData = (infoType, searchParams) => {
-    const url = new URL(BASE_URL + '/' + infoType);
-    url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+const getWeatherData = async (infoType, searchParams) => {
+	const url = new URL(BASE_URL + '/' + infoType);
+	url.search = new URLSearchParams({
+		...searchParams,
+		appid: API_KEY,
+	});
 
-    console.log(searchParams);
+	// const res = await fetch(url);
+	// return await res.json();
 
-    return fetch(url).then((res) => res.json());
+	return fetch(url).then((response) => response.json());
 };
 
 const formatCurrentWeather = (data) => {
-    const {
-        coord: { lat, lon },
-        main: { temp, feels_like, temp_min, temp_max, humidity },
-        name,
-        dt,
-        sys: { country, sunrise, sunset },
-        weather,
-        wind: { speed },
-    } = data;
+	const {
+		coord: { lon, lat },
+		main: { temp, feels_like, temp_min, temp_max, humidity },
+		name,
+		dt,
+		sys: { country, sunrise, sunset },
+		weather,
+		wind: { speed },
+	} = data;
 
-    const { main: details, icon } = weather[0];
+	const { main: details, icon } = weather[0];
 
-    return {
-        lat,
-        lon,
-        temp,
-        feels_like,
-        temp_min,
-        temp_max,
-        humidity,
-        name,
-        dt,
-        country,
-        sunrise,
-        sunset,
-        details,
-        icon,
-        speed,
-    };
-};
-
-const formatForecastWeather = (data) => {
-    let { timezone, daily, hourly } = data;
-    daily = daily.slice(1, 6).map((d) => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, 'ccc'),
-            temp: d.temp.day,
-            icon: d.weather[0].icon,
-        };
-    });
-
-    hourly = hourly.slice(1, 6).map((d) => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
-            temp: d.temp,
-            icon: d.weather[0].icon,
-        };
-    });
-
-    return { timezone, daily, hourly };
+	return {
+		lat,
+		lon,
+		temp,
+		feels_like,
+		temp_min,
+		temp_max,
+		humidity,
+		name,
+		dt,
+		country,
+		sunrise,
+		sunset,
+		details,
+		icon,
+		speed,
+	};
 };
 
 const getFormattedWeatherData = async (searchParams) => {
-    const formattedCurrentWeather = await getWeatherData(
-        'weather',
-        searchParams
-    ).then(formatCurrentWeather);
+	const formattedCurrentWeather = await getWeatherData(
+		'weather',
+		searchParams
+	).then(formatCurrentWeather);
 
-    const { lat, lon } = formattedCurrentWeather;
+	const { lat, lon } = formattedCurrentWeather;
 
-    const formattedForecastWeather = await getWeatherData('onecall', {
-        lat,
-        lon,
-        exclude: 'current,minutely,alerts',
-        units: searchParams.units,
-    }).then(formatForecastWeather);
+	const formattedForecastWeather = await getWeatherData('weather', {
+		lat,
+		lon,
+		exclude: 'current,minutely,alerts',
+		units: searchParams.units,
+	});
 
-    return { ...formattedCurrentWeather, ...formattedForecastWeather };
+	return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
 
 //function with luxon library that formats the local time
 const formatToLocalTime = (
-    secs,
-    zone,
-    format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+	secs,
+	zone,
+	format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
 ) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 const iconUrlFromCode = (code) =>
-    `http://openweathermap.org/img/wn/${code}@2x.png`;
+	`http://openweathermap.org/img/wn/${code}@2x.png`;
 
 export default getFormattedWeatherData;
 
